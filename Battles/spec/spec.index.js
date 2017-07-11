ae3a80e6e7e5ef1011c032026b5c45a1573cecb5
gilbertjacke <gilbertjacke@gmail.com>
2017-07-11 10:08:52 +0100

    added babel

---
+++ Battles/spec/spec.index.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import battle from '../index/battle'
+
+
+describe('battle()', function(){
+  it('is a function', function(){
+    expect(battle).to.be.a(function);
+  });
+  it('takes two arguments', function(){
+    expect(battle.length).to.equal(2);
+  });
+});
