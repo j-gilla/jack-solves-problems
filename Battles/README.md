f4b1ba78ae646c27873d0ca7bb7bd54071869d87
gilbertjacke <gilbertjacke@gmail.com>
2017-07-13 21:13:31 +0100

    looping through array

--- Battles/README.md
+++ Battles/README.md
@@ -5,7 +5,7 @@ Groups of characters decided to make a battle. Help them to figure out wha group
 Rules:
 
 Each character have its own power: A = 1, B = 2, ... Y = 25, Z = 26
-Only capital chatacters can and will participate a battle.
+Only capital characters can and will participate a battle.
 Only two groups to a fight.
 Group whose total power (A + B + C + ...) is bigger wins.
 If the powers are equal, it's a tie.
--- Battles/index.js
+++ Battles/index.js
@@ -1,42 +1,29 @@
 function battle(x, y) {
-  // Lets the battle begin!
   //create an object of letters  name and values props
-  let chars ={
-    "A": 1,
-    "B": 2,
-    "C": 3,
-    "D": 4,
-    "E": 5,
-    "F": 6,
-    "G": 7,
-    "H": 8,
-    "I": 9,
-    "J": 10,
-    "K": 11,
-    "L": 12,
-    "M": 13,
-    "N": 14,
-    "O": 15,
-    "P": 16,
-    "Q": 17,
-    "R": 18,
-    "S": 19,
-    "T": 20,
-    "U": 21,
-    "V": 22,
-    "W": 23,
-    "X": 24,
-    "Y": 25,
-    "Z": 26
-  }
+  let chars ={"A":1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11,
+  "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18,
+  "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25,
+  "Z": 26}
+  //split down x and y into indvidual characters & remove any lowercase
+  let singleX = x.replace( /[^A-Z]/g, '' ).split("");
+  let singleY= y.replace( /[^A-Z]/g, '' ).split("");
 
-  for(var i in chars){
+  let result = 0;
+  //loop through x and y variables and return property from chars
+  singleX.forEach((el, i, char) =>{
+    console.log(el);
+    for(el in chars){
+      
+      result += chars
+      return result;
+    }
+  });
 
-  }
-console.log(chars[i])
+return result;
 }
 
 
-console.log(battle("ONE",'TWO'));
+
+console.log(battle("OnE",'TWO'));
 
 module.exports=battle;
