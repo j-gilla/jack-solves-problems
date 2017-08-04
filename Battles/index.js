950115b0167767b9ce1b1c9776641181ab099d21
gilbertjacke <gilbertjacke@gmail.com>
2017-08-04 23:40:49 +0100

    battles complete 6 tests passing

--- Battles/index.js
+++ Battles/index.js
@@ -1,25 +1,27 @@
 function battle(x, y) {
-  //create an object of letters  name and values props
+
   let chars ={"A":1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11,
   "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18,
   "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25,
   "Z": 26}
 
-  //split down x and y into indvidual characters & remove any lowercase
   let singleX = x.replace( /[^A-Z]/g, '' ).split("");
   let singleY= y.replace( /[^A-Z]/g, '' ).split("");
 
-  let result = 0;
-  //loop through x and y variables and return property from chars
-  singleX.map((el, i, arr) =>{
+  let xRes = 0; let yRes = 0;
 
+  singleX.map((el,i,arr) =>{xRes += chars[el];
+    return xRes;
+  });
+  singleY.map((el,i,arr) =>{yRes += chars[el];
+    return yRes;
   });
 
+  if(xRes > yRes) return x;
+  if(yRes > xRes) return y;
+  if(xRes === yRes) return 'Tie!'
 
-
-  return;
 }
-
 
 
 console.log(battle("OnE",'TWO'));
--- Battles/spec/spec.index.js
+++ Battles/spec/spec.index.js
@@ -9,4 +9,24 @@ describe('battle()', function(){
   it('takes two arguments', function(){
     expect(battle.length).to.equal(2);
   });
+  it('returns the correct answer when x is the longest word', function(){
+    let x = 'ONE';
+    let y = 'ABC';
+    expect(battle(x,y)).to.equal(x);
+  });
+  it('returns the correct answer when y is the longest word', function(){
+    let x = 'ONE';
+    let y = 'TWO';
+    expect(battle(x,y)).to.equal(y);
+  });
+  it('returns tie when both words the same length', function(){
+    let x = 'bollokcs';
+    let y = 'bollokcs';
+    expect(battle(x,y)).to.equal('Tie!');
+  });
+  it('removes lowercase bitches from the mix', function(){
+    let x = 'OnE';
+    let y = 'TWO';
+    expect(battle(x,y)).to.equal(y);
+  });
 });
